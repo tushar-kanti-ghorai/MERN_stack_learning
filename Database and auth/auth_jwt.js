9b5e23abf07159313df1d10a57c749fd3f10217c
tushar-kanti-ghorai <tusharkantighorai643@gmail.com>
2025-02-01 12:39:15 +0530

    Done

--- Database and auth/auth_jwt.js
+++ Database and auth/auth_jwt.js
@@ -6,10 +6,10 @@ const value=({
 })
 
 
-// const token=jwt.sign(value,"secret");
+const token=jwt.sign(value,"secreii");
 
-const verified=jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVHVzaGFyIiwicGFzc3dvcmQiOiIxMjMyNTQiLCJpYXQiOjE3MjIyMjk3MDV9.ncLhzjwTpEHGE-QXBVMCX_SRdDhf1LknsB2Q_vFn1ZY","secret");
+const verified=jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVHVzaGFyIiwicGFzc3dvcmQiOiIxMjMyNTQiLCJpYXQiOjE3MzgzOTM1OTR9.al1259gvzXP43cm7D_qmVkT4wvtcbY0GgStz6P5F03g","secreii");
 
 console.log(verified);
 
-// console.log(token);
+console.log(token);
